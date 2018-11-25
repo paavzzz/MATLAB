5d52d7352a2167310f41078ced53ba166234d687
Pavithra <pavithra.njn@gmail.com>
2018-11-25 11:59:41 -0800

    translation, palindromic/hairpin structure, termination signal detection

--- hairpins.m
+++ hairpins.m
@@ -4,24 +4,24 @@
 %
 % Description: This MATLAB function references an Excel spreadsheet to "translate" a basic mRNA sequence into a polypeptide sequence.
 %
-% Input: A string representing a DNA sequence
+% Input: A string representing a RNA sequence
 % Return Type: A string representing the polypeptide sequence
 %
 % Assumptions: The passed in mRNA sequence does not contain errors.for
 %
 % Notes: start and stop codons indicated
 
-function aminos = translate( DNA )
+function aminos = translate( RNA )
 
 [data, arr1]=xlsread('codons.xlsx','A:A');
 [data, arr2]=xlsread('codons.xlsx','B:B');
 aminos=string('');
 
-for i=[1:3:(length(DNA)-2)]
+for i=[1:3:(length(RNA)-2)]
 
     for j=[1:numel(arr1)]
 
-        if strcmp(DNA(i:(i+2)),string(arr1(j)))
+        if strcmp(RNA(i:(i+2)),string(arr1(j)))
 
             aminos=strcat(aminos,string(arr2(j)));
 
@@ -34,7 +34,9 @@ end
 
 %hairpins
 %
-% Description: This MATLAB function detects palindromic sequences separated by atleast 3 nucleotides in the middle. These signify that the mRNA or RNA sequence could potentially form a hairpin structure, which is very common.
+% Description: This MATLAB function detects palindromic sequences separated by atleast 3 nucleotides in the middle. 
+% These signify that the mRNA or RNA sequence could potentially form a hairpin structure, which is very common.
+% It also checks to see if this hairpin structure is a part of the termination signal in bacteria (rho-independent)
 %
 % Input: A string representing a RNA sequence
 % Return Type: A string highlighting the palindromic part of the sequence
@@ -43,24 +45,27 @@ end
 %
 
          
-function sequence = hairpins(DNA)
+function sequence = hairpins(RNA)
          count = 0;
          i = 0;
-         j = length(DNA)+1;
-    sequence = []
-    while i != j && i<(length(DNA)./2) && j>(length(DNA)./2)
+         j = length(RNA)+1;
+    sequence = {};
+    while i != j && i<(length(RNA)./2) && j>(length(RNA)./2)
          i = i+1;
          j = j-1;
-            if abs(DNA(j) - DNA(i)) == 20 || abs(DNA(j) - DNA(i)) == 4
+            if abs(RNA(j) - RNA(i)) == 20 || abs(RNA(j) - RNA(i)) == 4
                 count = count+1;
             end
             if count > 3 && (j-i) >3
-                sequences = [sequence; double(DNA((i-count+1):(j+count-1)))];
+                sequence{end+1}= [double(RNA((i-count+1):(j+count-1)))];
                 count = 0;
             end
     end
     if isempty(sequence)
-         fprintf('Cruciforms and palindromes in this sequence of DNA are not possible to form.\n');
+         fprintf('Cruciforms and palindromes in this sequence of RNA are not possible to form.\n');
+    end
+    if !isempty(sequence) && RNA( length(RNA)-2: length(RNA) ) == 'UUU'
+         fprintf('Rho-independent termination may result in bacteria.\n');
     end
          sequence = char(sequence);
 end
